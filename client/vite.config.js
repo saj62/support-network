9f3c0b430742efa63b9a1b66f414591b2c436a78
saj62 <sarannjac@gmail.com>
2026-06-19 22:05:55 -0500

    update sitemap

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "vite-plugin-sitemap";
+import articlePosts from "./src/data/articlePosts";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: "https://geriatricssupportnetwork.org",
+      dynamicRoutes: [
+        "/",
+        "/about",
+        "/articles",
+        "/expert-series",
+        "/senior-activities",
+        ...articlePosts.map((post) => `/articles/${post.id}`),
+      ],
+    }),
+  ],
 });
